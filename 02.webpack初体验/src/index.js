33d264aff27b8ad5bf88f5abc45d6d9bf39e54a5
pengjinghuan <pengjinghuan@sinohealth.cn>
2021-07-19 11:50:32 +0800

    mm

--- 02.webpack初体验/src/index.js
+++ 02.webpack初体验/src/index.js
@@ -15,8 +15,9 @@
 
 // import "./index.css";
 
-import myData from "./data.json";
-console.log(myData);
+// import myData from "./data.json";
+// console.log(myData);
+// console.table(myData.fruitList);
 
 function add(x, y) {
   return x + y;
